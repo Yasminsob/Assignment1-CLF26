d8b7c16e2b1c231371577971920cbcd226f3e940
yasmin <yasminesobaihi@gmail.com>
2026-09-21 15:39:00 -0400

    Initial website upload

---
+++ readme.md
@@ -0,0 +1,11 @@
+This project takes the user on a little tour of my room
+In the first page, the user is in my room with images of furniture taken from the internet.
+Once the closet is clicked, the user moves to the other page.
+
+In the second page, the user gets to scroll between different tops and pants to pick. These images are my own.
+I used this website to aquire the slideshow:
+https://www.w3schools.com/w3css/w3css_slideshow.asp
+There is a text linking the user to the next page.
+
+In the third page, my bag is displayed with what i usually carry in it.
+There are short captions for whats relevant to the items.
---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+/// TOPS
+var slideIndex = 1;
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+
+    if (n > x.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = x.length;
+    }
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    x[slideIndex - 1].style.display = "block";
+}
+
+showDivs(slideIndex);
+
+
+// BOTTOMS
+var slideIndex2 = 1;
+
+function plusDivs2(n) {
+    showDivs2(slideIndex2 += n);
+}
+
+function showDivs2(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides2");
+
+    if (n > x.length) {
+        slideIndex2 = 1;
+    }
+
+    if (n < 1) {
+        slideIndex2 = x.length;
+    }
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    x[slideIndex2 - 1].style.display = "block";
+}
+
+showDivs2(slideIndex2);
